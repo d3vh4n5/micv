f44dac17f25ba2b382ab6d927fb7f940de571aa0
Juan <juanangelbasgall@hotmail.com>
2023-11-13 02:10:53 -0300

    Mejorando tarjetas de exs

--- assets/vue/components/CardExs.js
+++ assets/vue/components/CardExs.js
@@ -11,9 +11,25 @@ const CardExs = {
                 <a :href=project.visit_url v-if="project.visit_url != ''">Visit</a>
             </div>
             <div class="front">
-                {{ project.title }}
+                <div class="img" :style="{ background: 'url(' + project.img + ')' }"></div>
+                <p>
+                    {{ project.title }}
+                </p>
                 
-                ⚛️
+                <div class="tech-container python" v-if="project.technology === 'Python'">
+                    <img class="tech-logo right" src="./assets/img/Python_logo_01.svg.png" alt="Python logo">
+                </div>
+                <div class="tech-container php" v-if="project.technology === 'php'">
+                    <img class="tech-logo" src="./assets/img/PHP-logo.svg.png" alt="php logo"> 
+                </div>
+
+                <div class="tech-container node" v-if="project.technology === 'React' || project.technology === 'Node'">
+                    <img class="tech-logo" src="./assets/img/JavaScript-Symbol.png" alt="JavaScript logo">
+                </div>
+
+                <div class="tech-container ux-ui" v-if="project.technology === 'ux ui'">
+                    <img class="tech-logo right" src="./assets/img/ux-ui-logo.svg" alt="UX UI logo">
+                </div>
             </div>
         </div>
     `,
